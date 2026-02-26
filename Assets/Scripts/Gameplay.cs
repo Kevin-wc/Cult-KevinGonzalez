f98b5deeb315133bd530128917f64902e9a0ce87
Kevin-wc <kevingonzalezr203@gmail.com>
2026-02-26 15:50:38 -0600

    asas

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -11,9 +11,8 @@ public class Gameplay : MonoBehaviour
     TMP_Text bossText;
     TMP_Text playerText;
     public GameObject gameOver;
+    public GameObject lose;
 
-    public float timeStart;
-    public float timeEnd;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -34,16 +33,27 @@ public class Gameplay : MonoBehaviour
             bossText.text = "Boss HP: " + (bossHP -= 10);
         }
 
-        float currentTime = Time.time;
-        float timePassed = currentTime - timeStart;
-        while (timeStart >= timeEnd)
+        timesofar = time.time >= 5f;
+        if (timesofar && playerHP != 0)
         {
             playerText.text = "Player HP: " + (playerHP -= 10);
         }
 
+
         if (bossHP == 0)
         {
             gameOver.SetActive(true);
+            Time.timeScale = 0f;
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                Time.timeScale = 0f;
+
+            }
+        }
+
+        if (playerHP == 0)
+        {
+            lose.SetActive(true);
             Time.timeScale = 0f;
             if (Keyboard.current.spaceKey.wasPressedThisFrame)
             {
